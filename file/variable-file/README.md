2ee00b54bf69c7ab133046a8f0eb9ce4ac2c877c
darrenp <darrenp@efstechnology.com>
2017-02-07 08:53:28 +0000

    Added writing/reading variable to file. Updated some of README files.

---
+++ file/variable-file/README.md
@@ -0,0 +1,21 @@
+#Write variable to file
+
+Write an variable in this case integer to file and read it back again.
+
+After you have run the executable try to view the contents of the file.
+
+```
+cat text.bin
+```
+
+You should find that the file looks empty i.e. there is no text visible, the number has been written to the file using a binary stream.
+
+Look at the manual for the methods that do the write and reading to file.
+
+```
+man 3 fwrite
+```
+
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+
+This method takes a pointer, the size of the item being pointed to, the number of items (used when passing in an array) and a file descriptor.
---
+++ file/variable-file/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  const char *path = "text.bin";
+  int i = 10;
+  
+  FILE *fp_write = fopen(path, "w");
+
+  if(fp_write == NULL) {
+    printf("Unable to open file for writing: '%s'\n", path);
+    exit(EXIT_FAILURE);
+  }
+
+  fwrite(&i, sizeof(int), 1, fp_write);
+
+  fclose(fp_write);
+
+  printf("Finished writing to file: '%s'\n", path);
+  
+  FILE *fp_read = fopen(path, "r");
+  
+  if(fp_read == NULL) {
+    printf("Unable to open file for reading: '%s'\n", path);
+    exit(EXIT_FAILURE);
+  }
+
+  int num;
+  fread(&num, sizeof(int), 1, fp_read);
+
+  fclose(fp_read);
+
+  printf("Number read from file: '%d'\n", num);
+  
+  exit(EXIT_SUCCESS);
+}
