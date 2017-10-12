a7798f9ac74838e4bb7591628108ecc570acb2df
darrenp <darrenp@efstechnology.com>
2017-10-12 09:30:36 +0100

    Finished make2.

---
+++ misc/make/make2/makefile2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymath.h"
+
+int main(int argc, char *argv[]) {
+  
+  int r = add(5, 10);
+
+  printf("result: '%d'\n", r);
+  
+  exit(EXIT_SUCCESS);
+}
