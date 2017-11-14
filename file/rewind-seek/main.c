ea83ab0ecb4944baf02f8fd706e1de318ed299b1
drd <darrenp@yahoo.co.uk>
2017-11-14 20:29:29 +0000

    File rewind and fseek.

---
+++ file/rewind-seek/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  const char *path = "test.txt";
+
+  FILE *fp_write = fopen(path, "w"); 
+
+  if(fp_write == NULL) {
+    printf("Could not open file for write: '%s'\n", path);
+    exit(EXIT_FAILURE);
+  }
+
+  fprintf(fp_write, "Hello World!\n");
+
+  fclose(fp_write);
+  printf("Finished writing to file: '%s'\n", path);
+
+  FILE *fp_read = fopen(path, "r"); 
+
+  if(fp_read == NULL) {
+    printf("Could not open file for reading: '%s'\n", path);
+    exit(EXIT_FAILURE);
+  }
+
+  char c;
+  while((c = getc(fp_read)) != EOF) {
+    putchar(c);
+  }
+
+  rewind(fp_read);
+
+  while((c = getc(fp_read)) != EOF) {
+    putchar(c);
+  }
+
+  fseek(fp_read, 6, SEEK_SET);
+
+  while((c = getc(fp_read)) != EOF) {
+    putchar(c);
+  }
+  
+  fclose(fp_read);
+  
+  exit(EXIT_SUCCESS);
+}
