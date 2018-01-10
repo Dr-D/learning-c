cec33aaa873d1fb4770fed8bba0abf7be50fa8ed
Dr-D <Dr-D@users.noreply.github.com>
2018-01-10 12:24:01 +0000

    Initial CGI in docker. (#29)

---
+++ cgi/apache-docker/formget.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("Content-type: text/html\n\n");
+  printf("<html><head></head><body>");
+  
+  printf("<H1>Simple Form!</H1><br><br>");
+  printf("<HR>");
+
+  printf("<FORM ACTION=\"/cgi-bin/get\" METHOD=\"GET\"> Command: <INPUT TYPE=\"text\" NAME=\"command\" SIZE=40> ");
+  printf("<P>");
+  printf("<INPUT TYPE=\"submit\" VALUE=\"Submit Form!\">");
+  printf("<INPUT TYPE=\"reset\"  VALUE=\"Clear Form\">");
+  printf("<P>");
+  printf("</FORM>");
+  printf("<HR>");
+  
+  printf("</body></html>");
+  return 0;
+}
---
+++ cgi/apache-docker/get.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("Content-type: text/plain\n\n");
+  //printf("<html><head></head><body>");
+  printf("FORM Get<br><br>");
+
+  char * query_string = getenv("QUERY_STRING");
+  printf("QUERY_STRING: '%s'<br>", (query_string == NULL)? "null" : query_string);
+  
+  //printf("</body></html>");
+  return 0;
+}
