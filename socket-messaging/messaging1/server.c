da8a3ffb038b01ab980b90b54f135beaba34c907
drd <darrenp@yahoo.co.uk>
2018-08-14 06:48:52 +0100

    Completed messaging1

--- socket-messaging/messaging1/server.c
+++ socket-messaging/messaging1/server.c
@@ -7,34 +7,54 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+
+static char shared_message[100];
+
+void *server(void *ptr);
 
 int main(int argc, char *argv[]) {
+  pthread_t server_thread;
+  int res = pthread_create(&server_thread, NULL, server, "SERVER_THREAD");
+  if (res) {
+    fprintf(stderr, "Error - pthread_create(), response code: '%d'", res);
+    exit(EXIT_FAILURE);
+  }
 
+  sprintf(shared_message, "Initial message\n");
+
+  while(1) {
+    time_t time_now;
+    time(&time_now);
+
+    printf("%s: %s\n", ctime(&time_now), shared_message);
+    sleep(1);
+  }
+}
+
+void *server(void *ptr) {
   struct addrinfo hints, *res;
   memset(&hints, 0 , sizeof hints);
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_protocol = 0;
 
   getaddrinfo("0.0.0.0", "12345", &hints, &res);
 
   int sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-  u_int yes=1;
-  /*Allows socket to forcibly bind to a port already in use */
+  u_int yes = 1;
   setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
- 
+
   bind(sd, res->ai_addr, res->ai_addrlen);
-     
-  /* join IPv4 multicast group */
+
   struct ip_mreq mreq;
-  mreq.imr_multiaddr.s_addr=inet_addr("225.0.0.37");
-  mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+  mreq.imr_multiaddr.s_addr = inet_addr("225.0.0.37");
+  mreq.imr_interface.s_addr = htonl(INADDR_ANY);
   setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
 
   while (1) {
-    char read_b[100];
-    memset(&read_b, 0, 100);
-    int n =  recvfrom(sd, read_b, 100, 0, res->ai_addr, &res->ai_addrlen);
-    printf("%s\n", read_b);
+    recvfrom(sd, shared_message, 100, 0, res->ai_addr, &res->ai_addrlen);
   }
 }
